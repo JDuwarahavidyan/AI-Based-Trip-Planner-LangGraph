8bd28bf2486c8cb1e0ddb14726e16756f4d94426
JDuwarahavidyan <duwarahavidyan_j_e22@engug.ruh.ac.lk>
2026-04-07 00:07:27 +0530

    feat: implement LangGraph workflow with tool integration for trip planning agent

--- agents/agentic_workflow.py
+++ agents/agentic_workflow.py
@@ -4,6 +4,10 @@ from langgraph.prebuilt import ToolNode, tools_condition
 from prompt_library.prompt import SYSTEM_PROMPT
 from utils.model_loader import ModelLoader
 
+from tools.weather_info_tool import WeatherInfoTool
+from tools.place_search_tool import PlaceSearchTool
+from tools.expense_calculator_tool import CalculatorTool
+from tools.currency_convertor_tool import CurrencyConverterTool
 
 class GraphBuilder:
     def __init__(self, model_provider: str = "groq"):
@@ -11,8 +15,24 @@ class GraphBuilder:
         self.llm = self.model_loader.load_llm()
         
         self.tools = []
+
+        # Initialize tools
+        self.weather_tool = WeatherInfoTool()
+        self.place_search_tool = PlaceSearchTool()
+        self.calculator_tool = CalculatorTool()
+        self.currency_tool = CurrencyConverterTool()
         
+        # Add tools to list
+        self.tools.extend([
+            *self.weather_tool.weather_tool_list,
+            *self.place_search_tool.place_search_tool_list,
+            *self.calculator_tool.calculator_tool_list,
+            *self.currency_tool.currency_converter_tool_list
+        ])
         
+        self.graph=None
+        
+        self.llm_with_tools = self.llm.bind_tools(self.tools)
         
         self.system_prompt = SYSTEM_PROMPT
 
